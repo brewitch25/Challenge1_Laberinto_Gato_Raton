53b506feaff6625256848987b6a1804674939503
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-03-31 23:35:07 -0300

    Se logro que el gato y raton se mueva de forma inteligente, se uso la funcion "calcular distancia" en la funcion min_max

--- MinMax.py
+++ MinMax.py
@@ -4,10 +4,10 @@ from terreno_de_juego import crear_terreno, mover_personaje, mostrar_terreno
 
 profundidad = 4
 
-estado_inicial = {"fila_raton": 1, 
-                  "columna_raton": 1,
-                  "fila_gato": 3,
-                  "columna_gato": 3,
+estado_inicial = {"fila_raton": 3, 
+                  "columna_raton": 3,
+                  "fila_gato": 1,
+                  "columna_gato": 1,
                   "terreno": crear_terreno()}
 
 
@@ -51,13 +51,13 @@ def mover_gato(estado_actual, direccion):
     nueva_fila, nueva_columna = mover_personaje(nuevo_estado_gato["terreno"], estado_actual["fila_gato"], 
                                                 estado_actual["columna_gato"], direccion)
     nuevo_estado_gato["fila_gato"] = nueva_fila
-    nuevo_estado_gato["columna_raton"] = nueva_columna
+    nuevo_estado_gato["columna_gato"] = nueva_columna
     return nuevo_estado_gato
 
 # Funcion de calculo de distancia (Manhattan), saber si se acerca o se aleja
 def calcular_distancia(estado_actual):
     distancia_filas = abs(estado_actual["fila_raton"] - estado_actual["fila_gato"])
-    distancia_columnas = abs(estado_actual["raton_columna"] - estado_actual["columna_gato"])
+    distancia_columnas = abs(estado_actual["columna_raton"] - estado_actual["columna_gato"])
     return distancia_filas + distancia_columnas
     
 
@@ -66,7 +66,7 @@ def calcular_distancia(estado_actual):
 def min_max(estado_actual, profundidad_actual, turno_max):
     # -- Caso base - Cuando termina el juego -- 
     if profundidad_actual == 0 or termino_juego(estado_actual):
-        return ganador_o_perdedor(estado_actual)
+        return calcular_distancia(estado_actual)
 
     # Turno maximizador 
     if turno_max:
@@ -89,39 +89,49 @@ def min_max(estado_actual, profundidad_actual, turno_max):
 
 estado_de_juego = copy.deepcopy(estado_inicial)    
 
+print("Terreno inicial")
+mostrar_terreno(estado_de_juego["terreno"])        
 
-for turno in range(1, 21):           # => Rango de 1 a 9, 4 turnos para cada personaje
+for turno in range(1, 15):           # => Rango de 1 a 9, 4 turnos para cada personaje
     print(f"Turno nº:{turno}")
+    # = (profundidad - turno + 1) % profundidad
 
     # Turno del maximizador(RATON)
-    if turno % 2 != 0: 
+    if turno % 2 != 0:
+        print(f"Turno del 🐭")
         max_valor_raton = float("-inf")
         movimiento_elegido = None 
         for direccion in opciones_direcciones:
+            # print(f"Evaluando direccion del raton: {direccion}")  
             estado_a_evaluar = mover_raton(estado_de_juego, direccion)  # => Crea un estado despues de moverse en una direccion
             resultado = min_max(estado_a_evaluar, profundidad, False)
+            # print(f"Resultado del raton: {resultado}")
 
             # Evaluamos si el movimiento a realizar, maximiza
             if resultado > max_valor_raton:
                 max_valor_raton = resultado
                 movimiento_elegido = direccion
         # Movemos al raton en la direccion elegida
-        estado_de_juego = mover_raton(estado_de_juego, movimiento_elegido)        
+        estado_de_juego = mover_raton(estado_de_juego, movimiento_elegido)  
+        print(f"Direccion elegido del raton: {movimiento_elegido}")      
 
     # Turno minimizador (GATO)
     else:
+        print("Turno del 🐱")
         min_valor_gato = float("inf")
         movimiento_elegido = None
-        for direccion in opciones_direcciones:
+        for direccion in opciones_direcciones:   
+            # print(f"Evaluando direccion del gato: {direccion}")  
             estado_a_evaluar = mover_gato(estado_de_juego, direccion)
             resultado = min_max(estado_a_evaluar, profundidad, True)
-
+            # print(f"Resultado del gato: {resultado}")
             #Evaluamos si el movimiento a realizar, minimiza
             if resultado < min_valor_gato:
                 min_valor_gato = resultado
                 movimiento_elegido = direccion  
         # MOvemos al gato con el movimiento con la direccion elegida
         estado_de_juego = mover_gato(estado_de_juego, movimiento_elegido)
+        print(f"Direccion elegido del gato: {movimiento_elegido}")      
 
 # Mostramos el tablero despues de cada movimiento
-    mostrar_terreno(estado_de_juego["terreno"])        # Preguntar si esta bien
+    mostrar_terreno(estado_de_juego["terreno"])       
